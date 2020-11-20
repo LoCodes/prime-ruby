8823d6c684fe79f0d871bddf8ac19b83c943bc41
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-20 14:33:12 -0800

    commit tree bf2c07eaa9b5b783109e627075f9dd41d0412b56

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+
+def prime?(number)
+    return false if !number.integer?
+    return false if number < 2 
+    return true if number == 2
+    (2..number-1).each {|int| return false if number % int == 0}
+    true 
+
+end 
+
+
+# prime?
+#   returns true for prime numbers
+#   returns false for non-prime numbers
